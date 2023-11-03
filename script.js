0a9160204b8bc3231cecb31bdae1a85211136c98
kapoorakshit <56119482+kapoorakshit@users.noreply.github.com>
2023-11-03 13:26:58 +0530

    hii

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ function setClock() {
   setRotation(minuteHand, minutesRatio)
   setRotation(hourHand, hoursRatio)
 }
-
 function setRotation(element, rotationRatio) {
   element.style.setProperty('--rotation', rotationRatio * 360)
 }
